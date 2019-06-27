f611e9c43857e3868ad0d47b69c45bbdf9401f6e
Eric Schles <ericschles@gmail.com>
2019-06-27 15:23:55 -0400

    adding generated_model

--- generate_model.py
+++ generate_model.py
@@ -1,7 +1,8 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+import joblib
 
 df = pd.read_csv("training_data.csv")
 linear_regression = LinearRegression()
 linear_regression.fit(df[["A", "B", "C", "D"]], df["target"])
-
+joblib.dump(linear_regression, "regression.joblib")
